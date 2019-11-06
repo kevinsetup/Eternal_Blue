7dd481d5059894131ff901072acddf282aeff632
ElmerWillie <57423639+ElmerWillie@users.noreply.github.com>
2019-11-05 23:21:18 -0500

    Update Operaciones.java

--- eternalblue.com.pe/src/utils/Operaciones.java
+++ eternalblue.com.pe/src/utils/Operaciones.java
@@ -18,6 +18,12 @@ public class Operaciones {
 			System.out.println("REGISTRO NO ENCONTRADO");
 		} else if (tipo == 3) {
 			System.out.println("REGISTROS VACIOS");
+		} else if (tipo == 4) {
+			System.out.println("CANTIDAD DE DIGITOS / CARACTERES INCORRECTO");
+		} else if (tipo == 5) {
+			System.out.println("SOLO DEBE CONTENER DIGITOS");
+		}else if (tipo == 6) {
+			System.out.println("REGISTRO NO ELIMINADO");
 		}
 
 	}
