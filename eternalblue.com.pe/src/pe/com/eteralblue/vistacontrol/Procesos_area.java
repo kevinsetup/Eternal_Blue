26a32f44509a3736cca9ba9f94bbe514de87a61c
keu <kevinsetup900@gmail.com>
2019-11-05 22:34:43 -0500

    Actualizacion

--- eternalblue.com.pe/src/pe/com/eteralblue/vistacontrol/Procesos_area.java
+++ eternalblue.com.pe/src/pe/com/eteralblue/vistacontrol/Procesos_area.java
@@ -4,7 +4,23 @@ import pe.com.eteralblue.modelo.Area;
 import utils.GlobalVars;
 
 public class Procesos_area {
-
+public static int buscar_area(String nombre) {
+		int posicion = -1;
+		boolean encontrado = false;
+		do {
+			posicion++;
+			if (nombre.equals(GlobalVars.areas.get(posicion).getNombreArea())) {
+				encontrado = true;
+			}
+		} while (encontrado == false && posicion< GlobalVars.areas.size()-1);
+		
+		if (encontrado == false) {
+			posicion = -1;
+		}
+		
+		return posicion;
+	}
+	
 	public static void registrar_area() {
 		String nombreA, respuesta = "N";
 		do {
@@ -15,7 +31,7 @@ public class Procesos_area {
 			Area area = new Area(GlobalVars.IdArea, nombreA);
 			GlobalVars.areas.add(area);
 			do {
-				System.out.print("¿Desea registrar otra Area? [S/N]: ");
+				System.out.print("Å¼Desea registrar otra Area? [S/N]: ");
 				respuesta = GlobalVars.leer.cadenaMayuscula();
 			} while (!respuesta.equals("S") && !respuesta.equals("N"));
 		} while (respuesta.equals("S"));
@@ -23,24 +39,58 @@ public class Procesos_area {
 	}
 
 	public static void editar_area() {
-
+		if (GlobalVars.areas.isEmpty()) {
+			Operaciones.error(2);
+		}else {
+			int posicion;
+			String respuesta="N";
+			System.out.println("----- EDITAR AREA ----------");
+			listar_area();
+			System.out.println("Nombre: ");
+			posicion = buscar_area(GlobalVars.leer.cadena());
+			if (posicion == -1) {
+				Operaciones.error(2);
+			}else {
+				System.out.println("Nombre Actual: " + GlobalVars.areas.get(posicion).getNombreArea());
+				System.out.println("Â¿Desea Modificar Area? [S/N] : ");
+				respuesta = GlobalVars.leer.cadenaMayuscula();
+				if (respuesta.equals("S")) {
+					System.out.println("Nuevo Nombre: ");
+					GlobalVars.areas.get(posicion).setNombreArea(GlobalVars.leer.cadena());
+					Operaciones.salto_lineas(2);
+				}
+			}
+			
+		}
 	}
 
 	public static void listar_area() {
 		int num = 0;
-		System.out.println("N° \tNOMBRE");
+		System.out.println("----- LISTA DE AREAS ----------");
+		System.out.println("NÂ° \tNOMBRE");
 		for (Area area : GlobalVars.areas) {
 			num++;
 			System.out.println(num + "\t" + area.getNombreArea());
 
 		}
-
 	}
 	
-	
 	public static void eliminar_area() {
-		listar_area();
-		
+		if (GlobalVars.areas.isEmpty()) {
+			Operaciones.error(2);
+		}else {
+			int posicion;
+			System.out.println("***** ELIMINAR AREA *****");
+			listar_area();
+			System.out.println("NOMBRE: ");
+			posicion = buscar_area(GlobalVars.leer.cadena());
+			if (posicion == -1 ) {
+				Operaciones.error(2);
+			}else {
+				GlobalVars.areas.remove(posicion);
+				System.out.println("** AREA ELIMINADA CORRECTAMENTE **");
+			}
+		}
 	}
 
 }
