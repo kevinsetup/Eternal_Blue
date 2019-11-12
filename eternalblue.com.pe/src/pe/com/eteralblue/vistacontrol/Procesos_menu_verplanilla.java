073e65e6ee2c0c581159516a5d10f988bec12c89
keu <kevinsetup900@gmail.com>
2019-11-12 09:14:44 -0500

    Actualización final
    nuevo jar

--- eternalblue.com.pe/src/pe/com/eteralblue/vistacontrol/Procesos_menu_verplanilla.java
+++ eternalblue.com.pe/src/pe/com/eteralblue/vistacontrol/Procesos_menu_verplanilla.java
@@ -13,7 +13,6 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.text.xml.xmp.PdfA1Schema;
 
 public class Procesos_menu_verplanilla {
 
